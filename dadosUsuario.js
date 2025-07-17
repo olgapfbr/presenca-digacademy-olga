3f7276a52388f1c00669d2a3eefdbd3f816fe848
olgapfbr <olgapf@gmail.com>
2025-07-17 15:48:00 -0300

    first commit

---
+++ dadosUsuario.js
@@ -0,0 +1,11 @@
+const nome = "Luiz Otavio";
+const sobrenome = " Miranda";
+const idade = 30;
+const peso = 84;
+const altura = 1.80;
+let anoAtual = 2024;
+
+const imc = peso / (altura * altura);
+const anoNasc = anoAtual - idade;
+
+console.log(`${nome}${sobrenome} tem ${idade} anos, pesa ${peso} kg.`);
