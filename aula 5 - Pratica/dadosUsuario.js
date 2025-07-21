9879c093b46d8a1a4f6b9e18666b7bcc324d1a64
olgapfbr <olgapf@gmail.com>
2025-07-21 14:35:40 -0300

    aula 5

---
+++ aula 5 - Pratica/dadosUsuario.js
@@ -0,0 +1,14 @@
+const nome = "Luiz Otavio";
+const sobrenome = "Miranda";
+const idade = 30;
+const peso = 84;
+const altura = 1.80;
+let anoAtual = 2024;
+
+const IMC = peso / (altura * altura);
+const anoNasc = anoAtual - idade;
+
+console.log(`${nome}${sobrenome} tem ${idade} anos, pesa ${peso} kg.`);
+console.log(`Ele tem ${altura}m de altura, e o IMC dele é de ${IMC}.`);
+console.log(`${nome.split("z")[0].toLowerCase()} ${sobrenome.toLowerCase()} nasceu em ${anoNasc}.`);
+
