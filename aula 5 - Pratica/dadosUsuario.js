6b1397dab67b16f47faa7759b01b830f2c52a068
olgapfbr <olgapf@gmail.com>
2025-07-21 15:34:02 -0300

    aula 5 - Pratica

--- aula 5 - Pratica/dadosUsuario.js
+++ aula 5 - Pratica/dadosUsuario.js
@@ -11,4 +11,4 @@ const anoNasc = anoAtual - idade;
 console.log(`${nome}${sobrenome} tem ${idade} anos, pesa ${peso} kg.`);
 console.log(`Ele tem ${altura}m de altura, e o IMC dele é de ${IMC}.`);
 console.log(`${nome.split("z")[0].toLowerCase()} ${sobrenome.toLowerCase()} nasceu em ${anoNasc}.`);
-
+// se eu quiser que ele divida no espaço, coloco ${nome.split(" ")
