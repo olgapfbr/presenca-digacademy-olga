080f152b4ca06184e3f5f2c3cfc48136463f28a8
olgapfbr <olgapf@gmail.com>
2025-07-24 15:25:30 -0300

    aula 6 - Operacoes e comparacoes

---
+++ aula 6 - Operacoes e comparacoes/miniQuiz.js
@@ -0,0 +1,18 @@
+//let a = 5
+//let b = 3
+//console.log(a > b)
+// true
+
+//let a = 10
+//let b = 5
+//console.log(a == b)
+// false
+//console.log(!(b < a))
+// true
+
+//6 % 2 == 0 E 6 > 3
+
+let a = 6
+let b = 3
+
+console.log((a % 2 === 0) && (a > b))
