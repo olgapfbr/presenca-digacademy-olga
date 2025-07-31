17c0d73c207e4cbdfb1949b46de2611b2a476385
olgapfbr <olgapf@gmail.com>
2025-07-31 15:39:26 -0300

    aula 8 com nome certo :)

---
+++ aula 8/exercicio2.js
@@ -0,0 +1,15 @@
+// O sistema precisa verificar o tipo de número informado. Exiba se o número é positivo, negativo ou zero.
+
+num = 0
+
+if (num < 0){
+      console.log("Este número é negativo.")
+    }
+    else 
+      if (num > 0){
+        console.log("Este número é positivo.")
+      }
+      else {
+        console.log("Este número é o zero!\n")
+      }
+
---
+++ aula 8/exercicio3.js
@@ -0,0 +1,4 @@
+// Faça um script que pergunte em que turno você estuda.
+// Receba um valor para turno como M-matutino, V-vespertino ou N-noturno.
+// Imprima a mensagem "Bom dia!", "Boa tarde!" ou "Boa noite" ou "Valor inválido!", conforme o caso.
+
