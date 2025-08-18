5f02e626a06e0f2b39ad6566425400b8fe7965f6
olgapfbr <olgapf@gmail.com>
2025-08-18 14:47:30 -0300

    Aula 13 Objetos

---
+++ aula 13 - objetos/exercicio.js
@@ -0,0 +1,24 @@
+// let cat = {
+//    name: "Platão",
+//    color: "marrom e branco",
+//    age: 11,
+// }
+
+// console.log(
+//    `O ` + cat.name + ` é ` + cat.color + ` e tem ` + cat.age + ` anos.`
+// )
+
+// cat.toy = "bolinha";
+// cat.age = 12;
+
+// console.log(cat)
+
+let dog = {
+    name: "Abu",
+    breed: "Lhasa apso",
+    bark: function(){
+        console.log("Woof!")
+    }
+}
+
+dog.bark()
